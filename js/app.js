8a52797bc922e61e83d2f78084b04a0286fe64d0
merrick1988 <merrick198@gmail.com>
2013-09-11 15:29:05 +0300

    add possibility to click

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 (function () {
 	'use strict';
-	var app = angular.module('mainModule', ['directives']);
+	var app = angular.module('mainModule', ['directives']).
+		controller('mainController', ['$scope', function($scope){
+			$scope.scales = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+		}]);
 	angular.module("directives", []);
 }());
--- js/directives/slideRange.js
+++ js/directives/slideRange.js
@@ -4,22 +4,74 @@
 	'use strict';
 	angular.module('directives')
 		.directive('ngRange', ['$compile',
-			function ($compile, $templateCache, $http, $timeout) {
+			function ($compile) {
 				return {
 					restrict: "A",
 					replace: true,
+					scope: {scales: '='},
 					template:
 						'<div class="range__container">' +
-							'<div class="range__scale"></div>'+
-							'<div class="range__bar">' +
-								'<div class="range__arrow range__left-arrow"></div>'+
-								'<div class="range__arrow range__right-arrow"></div>'+
+							'<div class="range__limit">{{scales[0]}}</div>'+
+							'<div class="range__bar">'+
+								'<div ng-repeat="scale in innerScales" class="range__scale-separator" ng-style="{width: scaleWidth}" ng-click="setHandlePosition($event, $index)"></div>'+
+								'<div class="range__scale-separator" ng-style="{width: scaleWidth}" ng-click="setHandlePosition($event, innerScales.length)"></div>'+
+								'<div class="range__handle" ng-style="{left: handlePosition}"></div>'+
+								'<div class="range__scale-tooltip" ng-style="{left: tooltipPosition}">{{currentStep}}</div>'+
 							'</div>'+
+							'<div class="range__limit">{{scales[scales.length-1]}}</div>'+
 						'</div>',
 					link: function (scope, element, attrs) {
+						var init, updateHandlePosition;
+						var scales = _.toArray(scope.scales),
+							handleWidth = element[0].children[1].children[1].offsetWidth,
+							tooltipWidth = element[0].children[1].children[2].offsetWidth,
+							range = angular.element(element[0].children[1])[0],
+							rangeWidth =  element[0].children[1].offsetWidth;
+						scope.currentStep = 5;
+						scope.innerScales = scales.slice(0, scales.length-1).slice(1);
+						scope.scaleWidth = Math.floor(rangeWidth / (scope.innerScales.length + 1)) + 'px';
 
-					console.log("fsdfasdfasdfasfd")
-				}
+
+						updateHandlePosition = function(step){
+							var shift =  parseInt(scope.scaleWidth) * step, // consider the shift based on the current step
+								handleShift =  shift - handleWidth / 2, //  move the handle to the left exactly half its width
+								tooltipShift = shift - tooltipWidth / 2;  //  move the tooltip to the left exactly half its width
+
+							scope.handlePosition = handleShift + "px";
+							scope.tooltipPosition = tooltipShift + "px";
+						};
+
+						scope.setHandlePosition = function(event, index){
+							var dif = event.clientX - range.offsetLeft,
+								currentLeftShift =  index *  parseInt(scope.scaleWidth);
+
+							if(index === 0){
+								scope.currentStep = 1;
+								updateHandlePosition(scope.currentStep);
+								return;
+							}
+							console.log(index)
+							if(index === scope.innerScales.length){
+								scope.currentStep = scope.innerScales.length;
+								updateHandlePosition(scope.currentStep);
+								return;
+							}
+							if(currentLeftShift +  parseInt(scope.scaleWidth) / 2 < dif){
+								scope.currentStep = index + 1;
+							} else if(currentLeftShift -  parseInt(scope.scaleWidth) / 2 > dif){
+								scope.currentStep = index - 1;
+							} else{
+								scope.currentStep = index;
+							}
+							updateHandlePosition(scope.currentStep);
+						}
+
+						init = function(){
+							updateHandlePosition(scope.currentStep);
+						}
+
+						init();
+					}
 			}
 		}]);
 }
